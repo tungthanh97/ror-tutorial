7849fd14422d98bbba353f2ef95937993d1e5591
tung <tung020197@gmail.com>
2022-03-08 01:23:15 +0700

    Validate user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,6 @@ class UsersController < ApplicationController
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
-
     respond_to do |format|
       if @user.save
         format.html { redirect_to user_url(@user), notice: "User was successfully created." }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_many :articles
+  validates :name, presence: true,
+                   length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = URI::MailTo::EMAIL_REGEXP
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { minimum: 3, maximum: 25 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
